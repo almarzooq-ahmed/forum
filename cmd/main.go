19613ab25efb035327b142e602b2c2461527e0da
aalmarzo <almarzooqahmed@gmail.com>
2024-08-11 18:25:47 +0300

    #01 | Ahmed | Initialize database

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"log"
+
+	"forum/pkg/database"
+)
+
+func main() {
+	// Initialize database
+	db, err := database.NewSQLiteDB("./forum.db")
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+
+	// Get the underlying *sql.DB instance
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Failed to get database instance: %v", err)
+	}
+	defer sqlDB.Close()
+}
